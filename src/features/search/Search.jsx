56fdbb43cc85afc844c3d9e7d2be407b89cbf74c
Edmon <ed.moussa4@gmail.com>
2023-10-28 20:54:56 +0300

    refactored jsx, replaced alert with jsx styled error

--- src/features/search/Search.jsx
+++ src/features/search/Search.jsx
@@ -7,14 +7,18 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 const Search = () => {
   const navigate = useNavigate();
+
   const [searchQuery, setSearchQuery] = useState("");
+  const [isMoreThanThree, setIsMoreThanThree] = useState(true);
+
   const [triggerSearchIp, { data: weatherIp }] =
     weatherApi.endpoints.getLocationByIp.useLazyQuery();
   const [triggerIp, { data: ip }] = ipApi.endpoints.getIp.useLazyQuery();
+
   function searchClickHandler(e) {
     e.preventDefault();
     if (searchQuery.length < 3) {
-      return alert("Pls provide at least 3 characters");
+      setIsMoreThanThree(false);
     } else {
       navigate(`results/${searchQuery}`);
     }
@@ -23,14 +27,21 @@ const Search = () => {
     e.preventDefault();
     triggerIp();
   }
+
   return (
-    <section className="grid place-content-center h-screen">
-      <span className="justify-self-center text-4xl font-bold mb-5 opacity-90 drop-shadow-xl">
-        Weather App
-      </span>
-      <div className="w-80 h-96 shadow-2xl rounded-xl">
-        <form className="flex flex-col items-center h-full justify-center gap-4">
-          <img src={Logo} width={200} className="mb-5" alt="" />
+    <section className="flex flex-col justify-center items-center h-[100vh]">
+      <div className="flex flex-col justify-center items-center gap-6 shadow-2xl rounded-xl w-[400px] h-[500px]">
+        <h1 className="text-3xl font-bold">Weather App</h1>
+        <img src={Logo} width={200} alt="Logo" />
+
+        <form className="flex flex-col items-center justify-center gap-4 w-3/4">
+          {isMoreThanThree ? (
+            ""
+          ) : (
+            <span className="text-red-500">
+              Enter at least three characters
+            </span>
+          )}
           <div className="w-3/4 relative">
             <span
               className="absolute right-[10px] top-[5px] hover:cursor-pointer"
@@ -43,13 +54,15 @@ const Search = () => {
             </span>
             <input
               type="text"
-              className="border-2 border-black border-opacity-60 w-full rounded-md py-1 px-2"
+              className={`border-2 border-black border-opacity-60 w-full rounded-md py-1 px-2 ${
+                isMoreThanThree ? "" : "border-red-500"
+              }`}
               onChange={(e) => setSearchQuery(e.target.value)}
               value={searchQuery}
             />
           </div>
           <button
-            className="w-3/4 bg-blue-700 text-white rounded-md py-1 active:opacity-40 transition-opacity"
+            className="w-3/4 bg-blue-700 hover:bg-blue-900 text-white rounded-md py-1 active:opacity-40 transition-all ease-in-out"
             onClick={searchClickHandler}
           >
             Search
