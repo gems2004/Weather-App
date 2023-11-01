b9ae845cb2342b7b4e46efe0c13ad1a004257a47
Georgesa98 <georgesalebe0@gmail.com>
2023-11-01 21:51:29 +0300

    .

--- src/features/api/api.js
+++ src/features/api/api.js
@@ -1,34 +1,30 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const weatherApi = createApi({
   reducerPath: "weatherApi",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.VITE_WEATHER_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.WEATHER_URL }),
   endpoints: (builder) => ({
     getLocationByIp: builder.query({
-      query: (ip) => `/ip.json?key=${process.env.VITE_API_KEY}&q=${ip}`,
+      query: (ip) => `/ip.json?key=${process.env.API_KEY}&q=${ip}`,
     }),
     getLocationByName: builder.query({
-      query: (search) =>
-        `/search.json?key=${process.env.VITE_API_KEY}&q=${search}`,
+      query: (search) => `/search.json?key=${process.env.API_KEY}&q=${search}`,
     }),
     getCurrentWeatherData: builder.query({
-      query: (city) =>
-        `/current.json?key=${process.env.VITE_API_KEY}&q=${city}`,
+      query: (city) => `/current.json?key=${process.env.API_KEY}&q=${city}`,
     }),
     getForecastWeatherData: builder.query({
       query: (city) =>
-        `/forecast.json?key=${process.env.VITE_API_KEY}&q=${city}&days=3`,
+        `/forecast.json?key=${process.env.API_KEY}&q=${city}&days=3`,
     }),
     getAstronomyData: builder.query({
       query: (city, date) =>
-        `/astronomy.json?key=${
-          import.meta.env.VITE_API_KEY
-        }&q=${city}&dt=${date}`,
+        `/astronomy.json?key=${process.env.API_KEY}&q=${city}&dt=${date}`,
     }),
   }),
 });
 export const ipApi = createApi({
   reducerPath: "ipApi",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.VITE_IP_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.IP_URL }),
   endpoints: (builder) => ({
     getIp: builder.query({
       query: () => `?format=json`,
